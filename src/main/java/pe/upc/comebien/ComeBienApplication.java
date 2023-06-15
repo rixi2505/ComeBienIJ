235006d3748c8d0781c31ad75011f0743be0ad13
rixi2505 <richard2505xd@gmail.com>
2023-06-15 11:10:04 -0500

    Initial commit

---
+++ src/main/java/pe/upc/comebien/ComeBienApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.comebien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComeBienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComeBienApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/upc/comebien/ComeBienApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.comebien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComeBienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
